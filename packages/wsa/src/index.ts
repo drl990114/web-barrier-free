2c50238d2c85edb4459ee1ff6c2cb6423ea3ff8a
halodong <1661089448@qq.com>
2021-10-23 22:45:10 +0800

    feat class Wbfwsa

--- packages/wsa/src/index.ts
+++ packages/wsa/src/index.ts
@@ -1,1 +1,34 @@
-export const test = 'test'
+class Wbfwsa {
+  public target
+  public language
+  private readonly handler
+
+  constructor (options) {
+    this.target = null
+    this.language = 'zh-CN'
+    this.handler = (e) => {
+      if (e.target !== this.target && e.target !== document.body) {
+        this.target = e.target
+        console.log('tag', e.target.tagName)
+        console.log('text', e.target.innerText)
+        this.playAudio(e.target.innerText)
+      }
+    }
+  }
+
+  open (): void {
+    document.addEventListener('mousemove', this.handler)
+  }
+
+  close (): void {
+    document.removeEventListener('mousemove', this.handler)
+  }
+
+  playAudio (String): void {
+    const msg = new SpeechSynthesisUtterance()
+    msg.text = String
+    msg.lang = this.language
+    speechSynthesis.speak(msg)
+  }
+}
+export default Wbfwsa
