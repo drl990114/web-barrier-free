12c17dbb8a7f1ea3d754a59d4b787cc26dc156d5
halodong <1661089448@qq.com>
2021-10-24 23:25:23 +0800

    feat: add showbardom

--- packages/wsa/src/index.ts
+++ packages/wsa/src/index.ts
@@ -1,8 +1,11 @@
+const showBarDomId = '$$wsashowbar'
+
 class Wbfwsa {
   public target
   public language: string
   public rate: number
   public pitch: number
+  public showBarEl: HTMLDivElement | null = null
   private readonly utterThis: SpeechSynthesisUtterance | null = null
   private readonly handler
 
@@ -15,16 +18,20 @@ class Wbfwsa {
       if (e.target !== this.target && e.target !== document.body) {
         this.target = e.target
         this.playAudio(e.target.innerText)
+        if (this.showBarEl != null) this.showBarEl.innerText = e.target.innerText
       }
     }
   }
 
   open (): void {
+    const showBar = this.createShowBarDom()
+    this.showBarEl = showBar
     document.addEventListener('mousemove', this.handler)
   }
 
   close (): void {
     document.removeEventListener('mousemove', this.handler)
+    this.removeShowBarDom()
   }
 
   playAudio (String): void {
@@ -33,6 +40,31 @@ class Wbfwsa {
     msg.lang = this.language
     speechSynthesis.cancel()
     speechSynthesis.speak(msg)
+  }
+
+  createShowBarDom (): HTMLDivElement {
+    const prev = document.getElementById(showBarDomId) as HTMLDivElement
+    if (prev != null) return prev
+    const showBar = document.createElement('div')
+    showBar.id = showBarDomId
+    showBar.style.position = 'fixed'
+    showBar.style.bottom = '0px'
+    showBar.style.left = '0px'
+    showBar.style.width = '100%'
+    showBar.style.height = '80px'
+    showBar.style.fontSize = '32px'
+    showBar.style.fontWeight = 'bold'
+    showBar.style.textAlign = 'center'
+    showBar.style.lineHeight = '80px'
+    showBar.style.border = '2px solid #eee'
+    document.body.appendChild(showBar)
+    return showBar
+  }
+
+  removeShowBarDom (): void {
+    if (this.showBarEl != null) {
+      this.showBarEl.remove()
+    }
   }
 }
 export default Wbfwsa
