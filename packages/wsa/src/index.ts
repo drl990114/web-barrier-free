5faf2da132a0596cce1b004df54364560952ec96
halodong <1661089448@qq.com>
2021-10-26 22:58:05 +0800

    feat: mousehover emphasize style

--- packages/wsa/src/index.ts
+++ packages/wsa/src/index.ts
@@ -1,4 +1,7 @@
+import './index.css'
+
 const showBarDomId = '$$wsashowbar'
+const emphasizeClassName = 'emphasizeStyle'
 
 class Wbfwsa {
   public target
@@ -6,40 +9,73 @@ class Wbfwsa {
   public rate: number
   public pitch: number
   public showBarEl: HTMLDivElement | null = null
-  private readonly utterThis: SpeechSynthesisUtterance | null = null
-  private readonly handler
+  private readonly overHandler
+  private readonly outHandler
 
   constructor (options) {
     this.target = null
     this.language = 'zh-CN'
     this.rate = 1
     this.pitch = 1
-    this.handler = (e) => {
-      if (e.target !== this.target && e.target !== document.body) {
+    this.overHandler = (e: { target: HTMLElement }) => {
+      if (
+        e.target !== document.body &&
+        e.target.tagName.toLowerCase() !== 'html'
+      ) {
         this.target = e.target
-        this.playAudio(e.target.innerText)
-        if (this.showBarEl != null) this.showBarEl.innerText = e.target.innerText
+        this.emphasize(e.target)
+        const descriptionTag = this.descriptionTag(e.target.tagName.toLowerCase())
+        const text = descriptionTag !== null ? `${descriptionTag}: ${e.target.innerText}` : e.target.innerText
+        this.playAudio(text)
+        if (this.showBarEl != null) {
+          this.showBarEl.innerText = text
+        }
       }
+    }
+    this.outHandler = (e: { target: HTMLElement }) => {
+      this.removeEmphasize(e.target)
     }
   }
 
   open (): void {
     const showBar = this.createShowBarDom()
     this.showBarEl = showBar
-    document.addEventListener('mousemove', this.handler)
+    document.addEventListener('mouseover', this.overHandler)
+    document.addEventListener('mouseout', this.outHandler)
   }
 
   close (): void {
-    document.removeEventListener('mousemove', this.handler)
+    document.removeEventListener('mouseover', this.overHandler)
+    document.removeEventListener('mouseout', this.outHandler)
     this.removeShowBarDom()
   }
 
-  playAudio (String): void {
+  playAudio (String: string): void {
     const msg = new SpeechSynthesisUtterance()
     msg.text = String
     msg.lang = this.language
     speechSynthesis.cancel()
     speechSynthesis.speak(msg)
+  }
+
+  emphasize (el: HTMLElement): void {
+    el.classList.add(emphasizeClassName)
+  }
+
+  removeEmphasize (el: HTMLElement): void {
+    el.classList.remove(emphasizeClassName)
+  }
+
+  descriptionTag (tagName: string): string | null {
+    switch (tagName) {
+      case 'a':
+      case 'nav':
+        return '链接'
+      case 'image':
+        return '图片'
+      default:
+        return null
+    }
   }
 
   createShowBarDom (): HTMLDivElement {
