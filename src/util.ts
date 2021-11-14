3fb26bf8f7a3a70e6b92a105b6bc7ad8af175669
halodong <1661089448@qq.com>
2021-11-14 22:20:53 +0800

    fix: getElText only description 'zh-CN' tagName

--- src/util.ts
+++ src/util.ts
@@ -1,30 +1,48 @@
-export const getElText = (el: HTMLElement): string => {
-  const tag = descriptionTag(el.tagName)
+// const
+const cnGather = {
+  a: '链接',
+  img: '图片',
+  nav: '链接'
+}
+const enGather = {
+  a: 'Link',
+  img: 'Image',
+  nav: 'Link'
+}
+const lowerCaseImgTagName = 'img'
+const modes = ['finger', 'continuous']
+
+// fn
+const getGather = (language?: string): Object => {
+  let gather = cnGather
+  if (language === 'en') {
+    gather = enGather
+  }
+  return gather
+}
+
+export const getElText = (el: HTMLElement, language?: string): string => {
+  const tag = descriptionTag(el.tagName, language)
   const notContainChildText = getNotContainChildText(el)
   const text =
     tag !== null ? `${tag}: ${notContainChildText}` : notContainChildText
   return text
 }
 
-export const descriptionTag = (tagName: string): string | null => {
+export const descriptionTag = (tagName: string, language?: string): string | null => {
   const tag = tagName.toLowerCase()
-  switch (tag) {
-    case 'a':
-    case 'nav':
-      return '链接'
-    case 'image':
-      return '图片'
-    default:
-      return null
-  }
+  const gather = getGather(language)
+  return gather[tag] !== undefined ? gather[tag] : null
 }
 
 export const getNotContainChildText = (el: HTMLElement): string => {
+  if (el.tagName.toLowerCase() === lowerCaseImgTagName) {
+    return (el as HTMLImageElement).alt
+  }
   const notContainChildText: string = Array.prototype.filter.call(el.childNodes, (node: { nodeType: number }) => node.nodeType === 3).map(node => node.nodeValue.trim()).join('')
   return notContainChildText
 }
 
 export const testReadMode = (mode: string): boolean => {
-  const modes = ['finger', 'continuous']
   return Array.prototype.includes.call(modes, mode)
 }
