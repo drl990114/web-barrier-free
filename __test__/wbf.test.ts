cc730a4130a0f365d230752d8790b2de1d1d9e31
halodong <drl990114@gmail.com>
2022-05-16 20:54:41 +0800

    chore: Improve test cases

--- __test__/wbf.test.ts
+++ __test__/wbf.test.ts
@@ -1,9 +1,71 @@
+import { showBarDomId } from './../src/util'
 import Wbf from '../src/index'
 
 describe('test Wbf class', () => {
   it('should be singleton', () => {
+    Wbf.clearInstance()
     const instance1 = Wbf.getInstance()
     const instance2 = Wbf.getInstance()
     expect(instance1 === instance2).toBe(true)
+  })
+  it('When it executes open or close, it should render or remove the corresponding element by default, and change its state', () => {
+    Wbf.clearInstance()
+    const wbf = Wbf.getInstance()
+    wbf.open()
+    expect(wbf.opening).toBe(true)
+    expect(wbf.readMode).toBe('finger')
+    expect(document.querySelectorAll('.consoleEl-main').length).toBe(1)
+    expect(document.getElementById(showBarDomId)).not.toBe(null)
+    expect(wbf.volume).toBe(1)
+    expect(wbf.pitch).toBe(1)
+    expect(wbf.rate).toBe(1)
+    expect(wbf.language).toBe('zh-CN')
+    wbf.close()
+    expect(wbf.opening).toBe(false)
+    expect(document.querySelectorAll('.consoleEl-main').length).toBe(0)
+    expect(document.getElementById(showBarDomId)).toBe(null)
+  })
+  it('When passing different options, there should be different properties', () => {
+    Wbf.clearInstance()
+    const wbf = Wbf.getInstance({
+      language: 'en',
+      rate: 0.5,
+      pitch: 0.5,
+      volume: 0.5,
+      readMode: 'finger'
+    })
+    expect(wbf.volume).toBe(0.5)
+    expect(wbf.pitch).toBe(0.5)
+    expect(wbf.rate).toBe(0.5)
+    expect(wbf.language).toBe('en')
+  })
+  it('It should be possible to change the wbf attribute via the changeoption method and the limit value cannot be exceeded', () => {
+    Wbf.clearInstance()
+    const wbf = Wbf.getInstance()
+    expect(wbf.volume).toBe(1)
+    expect(wbf.pitch).toBe(1)
+    expect(wbf.rate).toBe(1)
+    expect(wbf.language).toBe('zh-CN')
+    wbf.setOption('volume', 0.5)
+    wbf.setOption('pitch', 0.5)
+    wbf.setOption('rate', 0.5)
+    wbf.setOption('language', 'en')
+    expect(wbf.volume).toBe(0.5)
+    expect(wbf.pitch).toBe(0.5)
+    expect(wbf.rate).toBe(0.5)
+    expect(wbf.language).toBe('en')
+    wbf.setOption('volume', 20)
+    wbf.setOption('pitch', 20)
+    wbf.setOption('rate', 20)
+    expect(wbf.volume).toBe(1)
+    expect(wbf.pitch).toBe(2)
+    expect(wbf.rate).toBe(10)
+  })
+  it('When calling changeMode, there should be a corresponding property change', () => {
+    Wbf.clearInstance()
+    const wbf = Wbf.getInstance()
+    expect(wbf.readMode).toBe('finger')
+    wbf.changeMode('continuous')
+    expect(wbf.readMode).toBe('continuous')
   })
 })
