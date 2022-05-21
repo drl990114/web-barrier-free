01bae170562bd224fb3971d2a7211b684997926a
halodong <drl990114@gmail.com>
2022-05-21 21:50:00 +0800

    test: add test case

--- __test__/wbf.test.ts
+++ __test__/wbf.test.ts
@@ -1,14 +1,17 @@
-import { showBarDomId } from './../src/util'
+import { emphasizeClassName, showBarDomId } from './../src/util'
 import Wbf from '../src/index'
 
 describe('test Wbf class', () => {
   it('should be singleton', () => {
+    expect.assertions(1)
     Wbf.clearInstance()
     const instance1 = Wbf.getInstance()
     const instance2 = Wbf.getInstance()
     expect(instance1 === instance2).toBe(true)
   })
+
   it('When it executes open or close, it should render or remove the corresponding element by default, and change its state', () => {
+    expect.assertions(11)
     Wbf.clearInstance()
     const wbf = Wbf.getInstance()
     wbf.open()
@@ -25,7 +28,9 @@ describe('test Wbf class', () => {
     expect(document.querySelectorAll('.consoleEl-main').length).toBe(0)
     expect(document.getElementById(showBarDomId)).toBe(null)
   })
+
   it('When passing different options, there should be different properties', () => {
+    expect.assertions(4)
     Wbf.clearInstance()
     const wbf = Wbf.getInstance({
       language: 'en',
@@ -39,7 +44,9 @@ describe('test Wbf class', () => {
     expect(wbf.rate).toBe(0.5)
     expect(wbf.language).toBe('en')
   })
+
   it('It should be possible to change the wbf attribute via the changeoption method and the limit value cannot be exceeded', () => {
+    expect.assertions(11)
     Wbf.clearInstance()
     const wbf = Wbf.getInstance()
     expect(wbf.volume).toBe(1)
@@ -61,11 +68,24 @@ describe('test Wbf class', () => {
     expect(wbf.pitch).toBe(2)
     expect(wbf.rate).toBe(10)
   })
+
   it('When calling changeMode, there should be a corresponding property change', () => {
+    expect.assertions(2)
     Wbf.clearInstance()
     const wbf = Wbf.getInstance()
     expect(wbf.readMode).toBe('finger')
     wbf.changeMode('continuous')
     expect(wbf.readMode).toBe('continuous')
+  })
+
+  it('When calling emphasize method, the corresponding element should have a class name', () => {
+    expect.assertions(2)
+    Wbf.clearInstance()
+    const wbf = Wbf.getInstance()
+    const div = document.createElement('div')
+    wbf.emphasize(div)
+    expect(div.classList.contains(emphasizeClassName)).toBe(true)
+    wbf.removeEmphasize(div)
+    expect(div.classList.contains(emphasizeClassName)).toBe(false)
   })
 })
